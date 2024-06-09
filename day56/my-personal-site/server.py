b5167fdbf23a10c7089ee79d28e060c6024d4339
helene <helene.maja@hotmail.dk>
2024-06-09 12:05:34 +0200

    completed serving static files using flask

--- day56/my-personal-site/server.py
+++ day56/my-personal-site/server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/") # declaring that this is the homepage
 def home():
-    return render_template("angela.htm")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
